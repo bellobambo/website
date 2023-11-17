808df710876f259387d0095c91030868ac8af42f
Bambo Bello <user@Bambo.local>
2023-11-17 23:24:23 +0100

    logical

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 
 <!-- <?php
-echo "i am loving php is a must <br>";
-echo 'its not bad so far'
+// echo "i am loving php is a must <br>";
+// echo 'its not bad so far'
 //  this is a comment
-?>
+// ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,40 +28,168 @@ Order a Pizza</button>
 <!-- VAR -->
 
 <?php
-$name = 'bambo. ';
-$food = 'pizza'  ;
-$email = 'bellobambo@gmail.com';
+// $name = 'bambo. ';
+// $food = 'pizza'  ;
+// $email = 'bellobambo@gmail.com';
 
-$gpa = '2.5';
-
-
-$age = 21;
-$users = 20;
-$quantity = 4;
-$tax_rate = 5.1;
-$price = 10;
-
-$online = false;
-$for_sale = false;
-$employed = true;
-$total = null;
+// $gpa = '2.5';
 
 
+// $age = 21;
+// $users = 20;
+// $quantity = 4;
+// $tax_rate = 5.1;
+// $price = 10;
 
-echo "you have ordered {$quantity} x {$food} <br> <br>";
+// $online = false;
+// $for_sale = false;
+// $employed = true;
+// $total = null;
 
-$total = $quantity * $price;
-echo "Your total is \${$total} <br>";
 
-echo "hello {$name} i love {$food} <br> with email of {$email} <br>" ;
-echo "you are {$age} years old";
 
-echo "we have {$users} online presently";
-echo "how many quantity of gold do you want {$quantity}?";
+// echo "you have ordered {$quantity} x {$food} <br> <br>";
 
-echo "your cgpa is \${$gpa} <br> <br>";
-echo "the sales task rate is {$tax_rate} <br>";
+// $total = $quantity * $price;
+// echo "Your total is \${$total} <br>";
 
-echo "this user is currently {$online}"
+// echo "hello {$name} i love {$food} <br> with email of {$email} <br>" ;
+// echo "you are {$age} years old";
 
+// echo "we have {$users} online presently";
+// echo "how many quantity of gold do you want {$quantity}?";
+
+// echo "your cgpa is \${$gpa} <br> <br>";
+// echo "the sales task rate is {$tax_rate} <br>";
+
+// echo "this user is currently {$online}"
+
+
+
+
+// $x = 10;
+// $y = 2;
+// $z = null;
+
+// // $z = $x + $y;
+// $z = $x % $y;
+
+
+// $counter = 0;
+
+// $counter+=2;
+// echo $counter;
+
+// $total = 1 + 2 - 3 * 4 / 5 ** 6;
+
+// echo $total;
+
+
+
+
+
+
+// ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <form action="index.php" method="post">
+
+        <label>radius:</label>
+        <input type="text" name='radius'>
+        <input type="submit" value="calculate">
+    </form> -->
+
+</body>
+
+</html>
+
+
+<?php
+
+$temp = 100;
+
+
+if ($temp >= 0 && $tem <= 30 ) {
+    echo "the weather is pleasant";
+}
+
+
+// $hours = 50;
+// $rate = 15;
+// $weekly_pay = null;
+
+
+// if ($hours <= 0) {
+//     $weekly_pay = 0;
+// } elseif ($hours <= 40) {
+//     $weekly_pay = "$hours * $rate";
+// }else{
+//     $weekly_pay = ($rate * 40) + ($hours - 40) * ($rate * 1.5);
+// }
+
+// echo "you made {$weekly_pay} this week alone";
+
+
+
+
+
+// $adult = false;
+// if($adult){
+//     echo"you may enter this site";
+// }else{
+//     echo "you must be an adult";
+// }
+
+// $age = -1;
+
+// if ($age >= 18) {
+//     echo "you may enter";
+// }
+// elseif( $age <= 0) {
+//     echo "not a valid age";
+// }
+// else{
+// echo "you must be 18";
+// }
+
+
+// $radius = $_POST["radius"];
+// $circumference = null;
+// $volume = null;
+
+// $circumference = 2 * pi() * $radius;
+// $circumference = round($circumference, 2);
+
+// $area = pi() *  pow($radius, 2);
+// $area = round($area, 2);
+
+
+// $volume = 4/3 * pi() * pow($radius, 3);
+// $volume = round($volume);
+
+// echo "Circumference = {$circumference}cm <br>";
+// echo "Area = {$area}cm^2 <br>";
+// echo "Volume = {$volume}";
+
+
+// $x = $_POST["x"];
+// $y = $_POST["y"];
+// $z = $_POST["z"];
+// $total = null;
+// $total = max($x, $y, $z);
+// $total = min($x, $y, $z);
+// $total = pow($x, $y);
+// $total = sqrt(10);
+// $total = rand(1, 10);
+// echo $total;
 ?>
