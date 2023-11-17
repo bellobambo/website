81bc8efee090672bce1e5347c5f393e181c66c15
Bambo Bello <user@Bambo.local>
2023-11-17 15:54:01 +0100

    php intro

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+echo "i am loving php is a must <br>";
+echo 'its not bad so far'
+//  this is a comment
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <br>    
+<button>
+Order a Pizza</button>
+</body>
+
+</html>
