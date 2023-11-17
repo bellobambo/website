3d003afce178510eb8cbd6f0b64014c8f0333564
Bambo Bello <user@Bambo.local>
2023-11-17 16:22:23 +0100

    var

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php
+<!-- INTRO -->
+
+
+<!-- <?php
 echo "i am loving php is a must <br>";
 echo 'its not bad so far'
 //  this is a comment
@@ -19,4 +22,46 @@ echo 'its not bad so far'
 Order a Pizza</button>
 </body>
 
-</html>
+</html> -->
+
+
+<!-- VAR -->
+
+<?php
+$name = 'bambo. ';
+$food = 'pizza'  ;
+$email = 'bellobambo@gmail.com';
+
+$gpa = '2.5';
+
+
+$age = 21;
+$users = 20;
+$quantity = 4;
+$tax_rate = 5.1;
+$price = 10;
+
+$online = false;
+$for_sale = false;
+$employed = true;
+$total = null;
+
+
+
+echo "you have ordered {$quantity} x {$food} <br> <br>";
+
+$total = $quantity * $price;
+echo "Your total is \${$total} <br>";
+
+echo "hello {$name} i love {$food} <br> with email of {$email} <br>" ;
+echo "you are {$age} years old";
+
+echo "we have {$users} online presently";
+echo "how many quantity of gold do you want {$quantity}?";
+
+echo "your cgpa is \${$gpa} <br> <br>";
+echo "the sales task rate is {$tax_rate} <br>";
+
+echo "this user is currently {$online}"
+
+?>
